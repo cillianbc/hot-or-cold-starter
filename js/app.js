fbfa9393d70c3b8e192f80de7c30aa1c62000ed0
Cillianbc <cillianbc@gmail.com>
2015-02-14 16:58:52 +1100

    Commented code, moved functions and variables inside document ready function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  /*--- Display information modal box ---*/
+  $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
 
-  	});
+  });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+    $(".overlay").fadeOut(1000);
+  });
+
+  /*--- user guess variable that takes value from input box ---*/
+  var userGuess = document.getElementById("userGuess").value;
+
+  /*--- On click function to run new Game function ---*/
+  $('.new').on('click',function(){
+    newGame()
+  });
+
+  /*--- New Game function that returns page to original state ---*/
+  var newGame = function(){
+    $('#count span').text('Test');
+    $('#guessList li').remove();
+  };
+
+ /*--- Function to create randomly generated number ---*/
+  var computerGuess = function(){
+    Math.floor((Math.random() * 100) + 1);
+  };
 
 });
-var userGuess = document.getElementById("userGuess").value;
 
-var newGame = function(){
-  $('#count span').text('Test');
-  $('#guessList li').remove();
-
-};
-
-var computerGuess = function(){
-  Math.floor((Math.random() * 100) + 1);
-};
 
 
