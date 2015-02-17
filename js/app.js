f827fe56813654380bf7655039e04527bca90b9c
Cillianbc <cillianbc@gmail.com>
2015-02-17 21:47:37 +1100

    New functions added, and it works

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
-	
-  /*--- Display information modal box ---*/
+
+    /*--- Display information modal box ---*/
   $(".what").click(function(){
     $(".overlay").fadeIn(1000);
 
@@ -12,25 +12,68 @@ $(document).ready(function(){
     $(".overlay").fadeOut(1000);
   });
 
-  /*--- Function to create randomly generated number ---*/
-  var computerGuess = function(){
-    Math.floor((Math.random() * 100) + 1);
-  };
-
-  /*--- user guess variable that takes value from input box ---*/
-  var userGuess = +$('#userGuess').val();
-  console.log(userGuess);
-
-
-  /*--- New Game function that returns page to original state ---*/
-  var newGame = function(){
+  function newGame(){
     $('.new').click(function(){
       $('#count').text('0');
+      $('#feedback').text("Make your Guess!");
       $('#guessList li').remove();
     });
   };
-  /*--- On click function to run new Game function ---*/
-  newGame();
+
+    /*--- Function to create randomly generated number ---*/
+  var computerGuess = Math.floor((Math.random() * 100) + 1);
+
+  var count = 1;
+  /*--- user guess variable that takes value from input box ---*/
+  function cleartext(){
+    $("#userGuess").attr("placeholder", "Enter your Guess").val('');
+  };
+
+  function numbGuess(count){
+    $('#count').text(count);
+  };
+
+  function guessAmount(guess){
+    $('#guessList').append('<li> '+guess+'</li>');
+  };
+
+  function feedback(guessText){
+    $('#feedback').text(guessText);
+  };
+
+  function game(guessdiff){
+    event.preventDefault();
+    newGame();
+    var userGuess = $('#userGuess').val();
+    var guessdiff = Math.abs(computerGuess - userGuess);
+
+    if (guessdiff === 0){
+      feedback("Correct");
+
+      guessAmount(userGuess);
+    }
+
+    else if (guessdiff <10){
+      feedback("Getting Warm");
+
+      guessAmount(userGuess);
+      numbGuess(count);
+      count++;
+    }
+
+    else {
+      feedback("Freezing");
+      guessAmount(userGuess);
+      numbGuess(count);
+
+      count++;
+    }
+  };
+
+  $("form").submit(function(event){
+  game()
+  cleartext();
+  });
 
 });
 
